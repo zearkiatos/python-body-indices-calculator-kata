6fb3025bf0d32e39b679d431a50218aec020d0b1
zearkiatos <caprilespe@outlook.com>
2022-07-23 12:16:13 -0400

    Calculator indices module

---
+++ README.md
@@ -0,0 +1,5 @@
+# Description
+This is a python 🐍 project kata 🥋. This application is to calculate the body indice 👤
+
+# Make with
+[![Python](https://img.shields.io/badge/python-2b5b84?style=for-the-badge&logo=python&logoColor=white&labelColor=000000)]()
---
+++ calculadora_indices.py
@@ -0,0 +1,51 @@
+''' 
+Calcula el índice de masa corporal de una persona a partir de la ecuación
+definida anteriormente.
+'''
+
+
+def calcular_IMC(peso: float, altura: float) -> float:
+    return 0.0
+
+
+'''
+Calcula el porcentaje de grasa de una persona a partir de la ecuación
+definida anteriormente.
+'''
+
+
+def calcular_porcentaje_grasa(peso: float, altura: float, edad: int, valor_genero: float) -> float:
+    return 0.0
+
+
+'''
+Calcula la cantidad de calorías que una persona quema estando en reposo
+(esto es, su tasa metabólica basal), a partir de la ecuación definida
+anteriormente.
+'''
+
+
+def calcular_calorias_en_reposo(peso: float, altura: float, edad: int, valor_genero: int) -> float:
+    return 0.0
+
+
+'''
+Calcula la cantidad de calorías que una persona quema al realizar algún tipo
+de actividad física (esto es, su tasa metabólica basal según actividad física),
+a partir de la ecuación definida anteriormente.
+'''
+
+
+def calcular_calorias_en_actividad(peso: float, altura: float, edad: int, valor_genero: float, valor_actividad: float) -> float:
+    return 0.0
+
+
+'''
+Calcula el rango de calorías recomendado, que debe consumir una persona
+diariamente en caso de que desee adelgazar, a partir de la ecuación
+definida anteriormente.
+'''
+
+
+def consumo_calorias_recomendado_para_adelgazar(peso: float, altura: float, edad: int, valor_genero: float) -> str:
+    return ''
